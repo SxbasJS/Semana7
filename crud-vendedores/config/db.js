02dbf874179ee23eb9e975999457d508df21fd21
Sebastian <Sxbas2005@gmail.com>
2025-05-09 09:32:51 -0500

    Cambiando credenciales BD

--- crud-vendedores/config/db.js
+++ crud-vendedores/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "mainline.proxy.rlwy.net",
-  user: "root",
-  password: "XtAZctRKYlqNezyoKlRlXMTrozOhDEsF",
-  database: "railway",
-  port: 48162,
+  host: "MYSQL1002.site4now.net",
+  user: "ab8bb9_ventas",
+  password: "sebastian12",
+  database: "db_ab8bb9_ventas",
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
